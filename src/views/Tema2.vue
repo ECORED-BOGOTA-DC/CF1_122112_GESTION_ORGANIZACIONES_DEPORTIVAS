d6ff48acc515489d1149d24dac6ea7f69e531fc1
luisEfe80 <luismendez80@hotmail.com>
2021-10-31 12:59:53 -0400

    Adicion de contenido

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -75,14 +75,134 @@
             figure.slideInLeft
               img(src='@/assets/curso/t2-i6.png' alt='imagen decorativa')
     p.mb-5  La siguiente tabla permite identificar de forma clara las etapas, definiciones y aspectos claves que se deben tener presentes para establecer un correcto proceso administrativo.
-    
 
+
+    .tabla-b.color-acento-botones.mb-5   
+      .tabla-b__header
+        h5.mb-0 Proceso administrativo
+      table
+        thead
+          tr
+            th Etapa
+            th Definición
+            th Componentes
+            th Tener en cuenta
+            th Aspectos claves
+        tbody
+          tr
+            td.text-bold Planeación
+            td Acción proactiva que busca prever anticipadamente qué espera alcanzar la organización y cuál es la ruta a seguir conforme a sus objetivos.
+            td 
+              ul.list-ul
+                li
+                  i.fas.fa-check
+                  p Definición de la estrategia, de los  objetivos y de las metas organizacionales.
+                li
+                  i.fas.fa-check
+                  p Definición de políticas, directrices.
+                li
+                  i.fas.fa-check
+                  p Construcción de programas.
+                li
+                  i.fas.fa-check
+                  p Diseño de procedimientos, métodos, normas y reglamentos.  
+                  
+            td 
+              p.mb-3  <b>Planeación estratégica:</b> a largo plazo, marca la ruta general de la organización (nivel gerencial).
+              p.mb-3  <b>Planeación táctica:</b> plan para un proceso, área o unidad de la organización (nivel coordinador, líder de proceso).
+              p.mb-3  <b>Planeación operativa:</b> construcción de un programa, de una tarea específica.
+            td 
+              p Nunca se tendrá certeza qué podrá pasar en el futuro, por lo que siempre existirá un nivel de incertidumbre. Pero, una adecuada planeación permitirá disminuir ese nivel de incertidumbre, tomar riesgos controlados encaminados a la organización y hacia el cumplimiento de sus objetivos.
+          tr
+            td.text-bold Organización
+            td Organizar y distribuir de manera lógica los recursos, con el fin de dar respuesta al cumplimiento de la planeación de la etapa anterior.
+            td 
+              ul.list-ul
+                li
+                  i.fas.fa-check
+                  p Construcción de la estructura funcional de la organización (organigrama).
+                li
+                  i.fas.fa-check
+                  p Asignación de responsables, funciones y responsabilidades.
+
+            td 
+              p.mb-3  Organización lineal o vertical.
+              p.mb-3  Organización funcional o estructural.
+            td 
+              p La estructura organizacional se establece según las características y estrategias de la organización. Su adecuada planeación facilitará la distribución eficiente del recurso para el cumplimiento de los objetivos. Se deberá contemplar los posibles riesgos en materia de conflictos de interés.
+         
+          tr
+            td.text-bold Dirección
+            td Guiar y alinear el cumplimiento del plan establecido de cada una de las partes de la organización.
+            td Aplicación de mecanismos de comunicación, liderazgo y motivación.
+            td Videoconferencias, reuniones presenciales o virtuales, equipos colaborativos, lecciones aprendidas, capacitaciones.
+            td 
+              p En esta etapa es de gran importancia el liderazgo, motivación y los mecanismos de comunicación establecidos para guiar y enrutar el talento humano al cumplimiento de los objetivos.
+          
+          tr
+            td.text-bold Control
+            td Velar por el cumplimiento del plan conforme a los objetivos, tiempos y recursos.
+            td 
+              ul.list-ul
+                li
+                  i.fas.fa-check
+                  p Evaluación de desempeño y resultado.
+                li
+                  i.fas.fa-check
+                  p Aplicación de listas de chequeo bajo estándar de calidad.
+                li
+                  i.fas.fa-check
+                  p Desarrollo de técnicas para el control como lo son, el Diagrama de Gantt y la Técnica Pert.
+            td  Auditorías, inspecciones, informes de gestión.
+            td  El control periódico se deberá aplicar para velar el cumplimiento del plan, según lo establecido e identificar posibles desviaciones para corregir a tiempo.
+          tr
+            td.text-bold Evaluación
+            td Verificar el cumplimiento de los objetivos conforme a lo estipulado en tiempo y recursos.
+            td Desarrollo de la autoevaluación del proceso y/o heteroevaluación realizada por un agente externo a la organización.
+            td  Evaluación del proceso involucrando a cada una de las partes interesadas: dirección, colaboradores, clientes – usuarios.
+            td  Es el cierre de un proceso donde se establecen propuestas de mejora para el inicio de un nuevo proceso.
+            
+    .row.mb-5
+      .col-lg-7(data-aos='slide-up')
+        p.mb-3  En la actualidad, las empresas cada vez son más competitivas y las organizaciones deportivas no son la excepción. Los clientes exigen servicios más especializados y de alta calidad, por lo que las organizaciones deberán trabajar para mejorar constantemente sus procesos.
+        p.mb-3  Una de las herramientas de gestión más empleadas en las empresas a nivel mundial es el Ciclo PHVA de mejora continua, en donde su principal característica es que no tiene un final en el momento en el que se obtiene un determinado resultado, sino que se crea un ciclo continuo en el que se reinicia una y otra vez, generando así un ciclo de mejora continua, ya que se parte de los aprendizajes del ciclo anterior para implementar por medio de mejoras en el actual. A continuación, se explica dicho ciclo:
+      .col-lg-5
+        figure(data-aos='flip-up')
+          img(src='@/assets/curso/t2-i7.png' alt='imagen decorativa')        
+
+    .row.mb-5.justify-content-center
+      .col-lg-8
+        ImagenInfografica.color-acento-contenido.mb-5
+          template(v-slot:imagen)
+            figure
+              img(src='@/assets/curso/t2-i8.svg', alt='imagen decorativa')
+
+          .tarjeta.color-acento-contenido.p-3(x="0%" y="20%")
+            p Se establecen objetivos, se identifican procesos conforme a las políticas y estrategias organizacionales.
+          .tarjeta.color-acento-contenido.p-3(x="100%" y="20%")
+            p Implementación de cambios y acciones de mejora planteadas en el ciclo anterior, enfocadas sl cumplimiento de los objetivos organizacionales.
+          .tarjeta.color-acento-contenido.p-3(x="0%" y="78%")
+            p Se establecen propuestas de mejora.             
+          .tarjeta.color-acento-contenido.p-3(x="100%" y="78%")
+            p Se mide, valora y verifica la efectividad de los cambios y mejoras implementadas.
+    
     Separador
     br
     br
     #t_2_2.titulo-segundo.color-secundario
       h2 2.2  Rol del gestor deportivo
+    .row.mb-5
+      .col-lg-7
+        p.mb-3  Luego de identificar el proceso administrativo se abordará el rol específico del gestor deportivo, que <b>consiste en el acompañamiento a todo el proceso administrativo orientado al cumplimiento de metas y estrategias organizacionales; es quien tiene la responsabilidad de liderar el proceso administrativo de la organización</b>. Debe tener una visión global que le permita conocer la empresa, su contexto, las necesidades de sus clientes, entender los objetivos organizacionales y establecer un plan para enrutar a las personas y los esfuerzos para el cumplimiento de las metas. Asimismo, debe establecer controles en el camino para determinar si va en la ruta correcta o se deben hacer ajustes en la marcha y, tener la capacidad de evaluar y establecer acciones que permitan una mejora continua de los procesos.
+        p.mb-3  El gestor deportivo requiere tener conocimientos tanto de temas afines a la gestión y todo lo relacionado con el proceso administrativo y los procesos de una organización; pero también, <b>es de gran importancia que el gestor deportivo conozca sobre el deporte</b>. A diferencia de empresas de otro sector el gestor no se puede limitar a conocimientos en materia de administración, deberá comprender y conocer todo lo inherente al deporte.
+      .col-lg-5
+        figure(data-aos='flip-up')
+          img(src='@/assets/curso/t2-i9.png' alt='imagen decorativa')
 
+    p.mb-5  El rol del gestor deportivo es orientar a la organización en el cumplimiento de sus objetivos y metas. Para tal fin, deberá velar por el óptimo funcionamiento de cada uno de los procesos organizacionales, como lo son:
+    figure
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 
 
